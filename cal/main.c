376d02d940a2a5aa0fc7b84b5a064f495678400c
lnaresh <leelanaresh.b@gmail.com>
2022-06-29 16:35:16 +0530

    added cal prj

---
+++ cal/main.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+
+#include"fh.h"
+
+main(){
+
+menu();
+
+}
---
+++ cal/menu.c
@@ -0,0 +1,8 @@
+#include"fh.h"
+void menu(void){
+printf("enter the menu");
+pritnf("this is for calculator")
+printf("this is for ADD");
+
+
+}
